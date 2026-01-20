f68b97c2cc4819c8e7befe16e8252e3223c77326
Inferato <infaustmager@gmail.com>
2026-01-20 20:04:05 +0200

    chore: added github CI

--- calendar_accounts/urls.py
+++ calendar_accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from calendar_accounts.views import manage_permissions, StrictLoginView
+from calendar_accounts.views import manage_permissions
 
 urlpatterns = [
     path('manage/<int:user_id>/', manage_permissions, name='manage_permissions'),
-    path('login/', StrictLoginView.as_view(), name='login')
 ]
